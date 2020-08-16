4b3731519f339cc152f5424c5873341e98cc4645
Proghammer <Proghammer@devs9.local>
2020-08-16 13:04:00 +0300

    build

--- webpack/Copy.js
+++ webpack/Copy.js
@@ -3,5 +3,5 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 module.exports = () => new CopyWebpackPlugin([
   { from: './src/fonts', to: '../dist/fonts' },
   { from: './src/favicon.ico', to: '../dist/favicon.ico' },
-  { from: './src/img', to: '../dist/img' }
+  { from: './src/images/icons', to: '../dist/src/images/icons' }
 ])
